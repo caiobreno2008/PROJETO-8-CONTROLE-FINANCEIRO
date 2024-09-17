7a357f05de2f3f3a310bd23876cd85f1bc9d28e4
caiobreno2008 <caiobreno155@gmail.com>
2024-09-16 21:47:35 -0300

    Update script.js
    corrigido

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
+let initialAmount = parseFloat(prompt("Digite a quantidade inicial de dinheiro disponível:"))
 
-function main() {
-    let initialAmount = parseFloat(prompt("Digite a quantidade inicial de dinheiro disponível:"))
-        if (initialAmount < 0) {
-        alert("Quantidade inicial inválida. O programa será encerrado.");
-        return
-    }
-    while (true) {
-                let action = prompt(`Quantidade de dinheiro: R$${initialAmount.toFixed(2)}\nEscolha uma opção:\n1. Adicionar dinheiro\n2. Remover dinheiro\n3. Sair`)
+
+if (isNaN(initialAmount) || initialAmount < 0) {
+    alert("Quantidade inicial inválida. O programa será encerrado.")
+} else {
+    do {
+       
+        let action = prompt(`Quantidade de dinheiro: R$${initialAmount.toFixed(2)}\nEscolha uma opção:\n1. Adicionar dinheiro\n2. Remover dinheiro\n3. Sair`);
+        
+        
         if (action === "1") {
-           
             let amountToAdd = parseFloat(prompt("Digite a quantidade a ser adicionada:"))
-            if (amountToAdd > 0) {
-                initialAmount += amountToAdd
+            
+            
+            if (!isNaN(amountToAdd) && amountToAdd > 0) {
+                initialAmount += amountToAdd;
             } else {
-                alert("Quantidade inválida. Tente novamente.");
+                alert("Quantidade inválida. Tente novamente.")
             }
         } else if (action === "2") {
-           
-            let amountToRemove = parseFloat(prompt("Digite a quantidade a ser removida:"));
-            if (amountToRemove > 0 && amountToRemove <= initialAmount) {
+            let amountToRemove = parseFloat(prompt("Digite a quantidade a ser removida:"))
+            
+            
+            if (!isNaN(amountToRemove) && amountToRemove > 0 && amountToRemove <= initialAmount) {
                 initialAmount -= amountToRemove;
             } else {
-                alert("Quantidade inválida ou insuficiente. Tente novamente.");
+                alert("Quantidade inválida ou insuficiente. Tente novamente.")
             }
         } else if (action === "3") {
-            
-            alert("voce encerrou o calculo.")
-            break;
+            alert("Você encerrou o cálculo.")
+            break 
         } else {
             alert("Opção inválida. Tente novamente.")
         }
-    }
+    } while (true) 
 }
-main()
